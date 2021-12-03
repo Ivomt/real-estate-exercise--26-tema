b7ec1c41d4933a994bccf19ad77be7d3c9755414
Ivomt <ivo_s5@abv.bg>
2021-12-03 06:33:05 +0200

    Initial commit

---
+++ src/main/java/com/aacademy/realestate24temaesercise/RealEstate24TemaEserciseApplication.java
@@ -0,0 +1,13 @@
+package com.aacademy.realestate24temaesercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstate24TemaEserciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstate24TemaEserciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aacademy/realestate24temaesercise/RealEstate24TemaEserciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aacademy.realestate24temaesercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstate24TemaEserciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
